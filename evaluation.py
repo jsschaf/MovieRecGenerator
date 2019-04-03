5a1bc2a09fe60ec5bd6649b12fcef5975926ea5c
Michelle M Gu <mmgu@umich.edu>
2019-04-03 17:52:31 -0400

    evaluation init

---
+++ evaluation.py
@@ -0,0 +1,10 @@
+"""
+EECS 486: Final Project
+
+Python implementation that takes preprocessed Twitter data and preprocessed
+movie keyword data as input, then uses a TF-IDF-based information retrieval
+system to identify top movies that a given user will find interesting
+depending on the person's likes and dislikes.
+
+NOTE: This program runs with Python 3.7.
+"""
