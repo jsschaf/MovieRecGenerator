5607d039c6b969d560905e5ff5ead682c4107ed6
Michelle M Gu <mmgu@umich.edu>
2019-04-08 19:37:52 -0400

    added json parsing and sentiment analysis

--- evaluation.py
+++ evaluation.py
@@ -11,12 +11,14 @@ NOTE: This program runs with Python 3.7.
 
 
 import collections
+import json
 import movies # Python library containing movie keyword identification 
 			  # and preprocessing.
-
+import os
 import tweets # Python library containing preprocessed Twitter data.
 
 from math import log10, sqrt
+from textblob import TextBlob
 
 
 def indexMovies(movie, invertedIndex): 
@@ -131,6 +133,42 @@ def retrieveMovies(tweet, invertedIndex):
 		retrieved[doc] = numerator / (denominatorDoc * denominatorQuery)
 	return retrieved
 
+
+if __name__ == '__main__':
+
+	# Open JSON file containing a list of tweets.
+	#
+	# listOfTweets is a dictionary-based format of the tweets that we will be
+	# using to suggest movies for.
+	tweetsFile = open(os.getcwd() + "/tweets.json", 'r')
+	tweetsContents = tweetsFile.read()
+	listOfTweets = json.loads(tweetsContents)
+
+	# Open JSON file containing a list of movies and their corresponding
+	# keywords.
+	moviesFile = open(os.getcwd() + "/movies.json", 'r')
+	moviesContents = moviesFile.read()
+	listOfMovies = json.loads(moviesContents)
+
+	# Put movie information into inverted index.
+	invertedIndex = {}
+	for movie in listOfMovies:
+		indexMovies(movie, invertedIndex)
+
+	# Because some Twitter users may express negative opinions about certain
+	# movies/topics, it would not be reasonable to make movie suggestions
+	# based on these types of opinions. This is because we do not know 
+	# what the user will like in terms of movies, only the types of movies
+	# that he or she does not like. Furthermore, suggesting an "opposite" to
+	# a given movie topic is not really realistic because there is no real
+	# definitive opposite to, say, action/adventure movies.
+	#
+	# We use sentiment analysis to filter out the negative tweets and only 
+	# use positive and neutral tweets for training and testing.
+	for tweet in listOfTweets:
+		results = TextBlob(tweet["tokens"]) # CHECK FIELD FOR TWEET TOKENS !!!!!!!!!!!!!!!!!!!!!!!!!!
+		if (results.sentiment[0] > 0) and (not (results.sentiment[0] < 0)):
+       		print(retrieveMovies(tweet, invertedIndex))
 
 
 
