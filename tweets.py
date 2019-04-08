42494312cf977fa78aa0cc06d27e5ed36fe42076
licurtis <licurtis@umich.edu>
2019-04-08 01:49:40 -0400

    processes tweets

--- tweets.py
+++ tweets.py
@@ -1,3 +1,4 @@
+import json
 import tweepy
 from tweepy import OAuthHandler
 
@@ -10,8 +11,63 @@ access_secret = 'McFpqARFMKYQZY9g85BRiQKUP72722PIciiMIwedK0HXZ'
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-for tweet in api.user_timeline(screen_name="thehill", count=5):
-    print(tweet.text)
-# print(api.get_user(screen_name="UMich"))
+def parseUser(u):
+    new_u = { key: u[key] for key in [
+        'created_at',
+        'description',
+        'entities',
+        'favourites_count',
+        'followers_count',
+        'friends_count',
+        'id',
+        'id_str',
+        'lang',
+        'listed_count',
+        'location',
+        'name',
+        'screen_name',
+        'statuses_count',
+        'url'
+        ]
+    }
+    return new_u
+
+def parseTweet(s):
+    new_s = { key: s[key] for key in [
+        'created_at',
+        'entities',
+        'favorite_count',
+        'id',
+        'id_str',
+        'lang',
+        'metadata',
+        'text',
+        'source'
+        ]
+    }
+    new_s['user'] = parseUser(s['user'])
+    return new_s
+
+def retrieveTweets():
+    tweets = []
+    query = '#movie'
+    limit = 100
+    # Maybe use random words for query?
+    with open('tweets.json', 'w') as file:
+        for i, tweet in enumerate(tweepy.Cursor(api.search, q=query, result_type='recent').items(limit)):
+                processed_tweet = parseTweet(tweet._json)
+                print(i, processed_tweet)
+                tweets.append(processed_tweet)
+                json.dump(processed_tweet, file, sort_keys=True, indent=2)
+                file.write(',\n')
+    return tweets
+
+# tweets = retrieveTweets()
+tweets = {}
+with open('tweets.json', 'r') as file:
+    tweets = json.load(file)
+
+for tweet in tweets:
+    print(tweet['text'])
