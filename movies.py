d6e614608d43903d7b42db559e68b9048597174a
Sukang Sabina Kim <sukang@umich.edu>
2019-04-18 11:06:38 -0400

    updated movies.py description

--- movies.py
+++ movies.py
@@ -1,36 +1,41 @@
-# python for movies
+"""
+EECS 486: Final Project
 
-# The Movie DB 
-# API Key (v3 auth): 68ecb8ff29c2d7356eb0b66d31136fd9
-# API Read Access Token ()
+Python implementation that requests movies from the Movie DB and stores movie data in movies.json
 
-# MUST have requests installed. (pip3 install requests)
-
+NOTE: This program runs with Python 3.7
+NOTE: This program requires python library "requests"
+NOTE: API Key (v3 auth): 68ecb8ff29c2d7356eb0b66d31136fd9
+"""
 import requests
 import time
 import os, sys
 import json
 
-# list of json
-# {"id": id, "name": name, "genre_list": [genres], "keyword_list": [keywords]}
+# list of movie data json
+# json format: {"id": id, "name": name, "genre_list": [genres], "keyword_list": [keywords]}
 movie_json_list = [] 
 
-movie_set = set() # prevent duplicated movies
+# prevent duplicated movies
+movie_set = set() 
 
+# dictionary that maps from genre ID to genre string 
 genre_dict = {27: "Horror", 28: "Action", 35: "Comedy", 99: "Documentary", 10749: "Romance"}
 
+# URL to get JSON from the movie DB 
 URL = "https://api.themoviedb.org/3/discover/movie"
+# API KEY - credentials
 API_KEY = "68ecb8ff29c2d7356eb0b66d31136fd9"
+# array of genres that we are not interested in looking at 
 WITHOUT_GENRES = "12,16,80,18,10751,14,36,10402,9648,878,10770,10752,37,53"
 
-it = 0
-# get keywords
+# function that get keywords given the movie id
+# returns an array of unprocessed list of keywords retrieved 
 def get_keywords(movie_id):
-	global API_KEY, it
-	it += 1
+	global API_KEY
 	KEYWORD_URL = "https://api.themoviedb.org/3/movie/" + str(movie_id) + "/keywords"
 	KEYWORD_PARAM = {"api_key" : API_KEY}
-	keyword_data = request_and_process(KEYWORD_URL, KEYWORD_PARAM)
+	keyword_data = request_and_retrieve(KEYWORD_URL, KEYWORD_PARAM)
 
 	keyword_results = keyword_data['keywords']
 	keywords_list = []
@@ -47,20 +52,19 @@ def get_keywords(movie_id):
 
 	return keywords_list
 
-def request_and_process(request_url, request_param):
-	global f
-
+# function that requests JSON and returns 
+def request_and_retrieve(request_url, request_param):
 	request = requests.get(url = request_url, params = request_param)
 	data = request.json()
+	# sleep if we exceed # requests per second
 	if 'status_code' in data:
-		print(data)
 		time.sleep(9) # reset counter
 		request = requests.get(url = request_url, params = request_param)
 		data = request.json()
 
 	return data
 
-
+# function that adds movie data to list of json  
 def add_movie_data(json_data):
 	# go through the movies
 	global genre_dict
@@ -87,30 +91,20 @@ def add_movie_data(json_data):
 
 
 if __name__ == '__main__':
-	tot_movies = 0
-	for year in range(2008, 2020): #TODO: change years 
+	for year in range(2008, 2020):  
 		PARAMS = {"api_key" : API_KEY, "vote_average.gte": 5, "primary_release_year": year, "without_genres": WITHOUT_GENRES, "vote_count.gte": 20, "with_original_language": "en"}
-		data = request_and_process(URL, PARAMS)
+		data = request_and_retrieve(URL, PARAMS)
 		results = data['results']
 		add_movie_data(results)
-		print("year: " + str(year) + " total moives: " + str(data["total_results"]))
-		tot_movies += data["total_results"]
-		print("cumultaive result: " + str(tot_movies))
 		# get next json data
 		total_pages = data["total_pages"]
 		for page in range(2, total_pages):
 			PAGE_PARAMS = {"api_key": API_KEY, "page": page,"vote_average.gte": 5, "primary_release_year": year, "without_genres": WITHOUT_GENRES, "vote_count.gte": 20, "with_original_language": "en"}
-			data = request_and_process(URL, PAGE_PARAMS)
+			data = request_and_retrieve(URL, PAGE_PARAMS)
 			results = data['results']
 			add_movie_data(results)
 
-	print(len(movie_json_list))
 	with open(os.getcwd()+'/movies.json', 'w+') as outfile:  
 	    json.dump(movie_json_list, outfile)
-
-
-
-
-
 
 
